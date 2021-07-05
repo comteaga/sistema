67077abfa797b9a0c891c37702397f2f94c29694
matheus <matheuscomteaga@hotmail.com>
2021-07-05 12:02:31 -0300

    telas de signIn e signUp

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,45 @@
+import './signIn.css';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import logo from '../../assets/logo.png';
+
 function SignIn() {
-  return <div>Tela de Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(e) {
+    //preventDefault faz com que a página não seja recarregada
+    e.preventDefault();
+    alert('clicou');
+  }
+
+  return (
+    <div className="container-center">
+      <div className="login">
+        <div className="logo-area">
+          <img src={logo} alt="Logo do Sistema" />
+        </div>
+        <form onSubmit={handleSubmit}>
+          <h1>Entrar</h1>
+          <input
+            type="text"
+            placeholder="email@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+          <input
+            type="password"
+            placeholder="********"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+          <button type="submit">Acessar</button>
+        </form>
+
+        <Link to="/register">Criar uma conta</Link>
+      </div>
+    </div>
+  );
 }
 
 export default SignIn;
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,5 +1,51 @@
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import logo from '../../assets/logo.png';
+
 function SignUp() {
-  return <div>Tela de cadastro</div>;
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(e) {
+    //preventDefault faz com que a página não seja recarregada
+    e.preventDefault();
+    alert('clicou');
+  }
+
+  return (
+    <div className="container-center">
+      <div className="login">
+        <div className="logo-area">
+          <img src={logo} alt="Logo do Sistema" />
+        </div>
+        <form onSubmit={handleSubmit}>
+          <h1>Cadastrar</h1>
+          <input
+            type="text"
+            placeholder="Seu Nome"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+          ></input>
+          <input
+            type="text"
+            placeholder="email@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+          <input
+            type="password"
+            placeholder="********"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+          <button type="submit">Acessar</button>
+        </form>
+
+        <Link to="/">Já tem uma conta? Entre</Link>
+      </div>
+    </div>
+  );
 }
 
 export default SignUp;
